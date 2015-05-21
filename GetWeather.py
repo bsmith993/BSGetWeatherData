012f2c4b897388c26f57b3586e3c3830e79cd867
bsmith993 <bsmith993@gmail.com>
2015-05-21 12:33:03 -0400

    Updated

--- GetWeather.py
+++ GetWeather.py
@@ -1,9 +1,40 @@
-import urllib2
+import urllib.request
 import json
-f = urllib2.urlopen('http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/IA/Cedar_Rapids.json')
-json_string = f.read()
-parsed_json = json.loads(json_string)
-location = parsed_json['location']['city']
-temp_f = parsed_json['current_observation']['temp_f']
-print "Current temperature in %s is: %s" % (location, temp_f)
-f.close()
+import datetime
+
+now = datetime.datetime.now()
+current_day = now.day
+
+response = urllib.request.urlopen(request)
+encoding = response.info().get_param('charset', 'utf8')
+data = json.loads(response.read().decode(encoding))
+
+current_temp = int(data['current_observation']['temp_f'])
+
+response = urllib.request.urlopen(request)
+encoding = response.info().get_param('charset', 'utf8')
+data = json.loads(response.read().decode(encoding))
+
+for period in data['forecast']['simpleforecast']['forecastday']:
+    if period['date']['day'] == current_day:
+        today_high = int(period['high']['fahrenheit'])
+        
+
+ISYServerIP = "192.168.1.144"
+ISYServerPort = "80"
+ISYUsername = "username"
+ISYPassword = "password"
+TopURL = 'http://'+ ISYServerIP + ':' + ISYServerPort
+SetCurrentWeatherURL = 'http://' + ISYServerIP + ':' + ISYServerPort + '/rest/vars/set/2/3/' + str(current_temp)
+SetTodayHighURL = 'http://' + ISYServerIP + ':' + ISYServerPort + '/rest/vars/set/2/4/' + str(today_high)
+
+password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+password_mgr.add_password(None, TopURL, ISYUsername, ISYPassword)
+handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+opener = urllib.request.build_opener(handler)
+opener.open(SetCurrentWeatherURL)
+
+password_mgr.add_password(None, TopURL, ISYUsername, ISYPassword)
+handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+opener = urllib.request.build_opener(handler)
+opener.open(SetTodayHighURL)
